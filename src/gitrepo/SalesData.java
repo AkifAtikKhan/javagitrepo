8a040c476e1caa1bbc58063ae999e8e7b74c9cce
Akif Khan <akhan15@winona.edu>
2018-01-21 15:16:56 -0600

    Initial Check-in of sales tools.

---
+++ src/gitrepo/SalesData.java
@@ -0,0 +1,18 @@
+package gitrepo;
+
+public class SalesData {
+
+	int data[] = {0,4,42};
+	
+	
+	
+	public void display()
+	{
+		System.out.println("TODO: Implement data display");
+	}
+	
+	
+	
+	
+	
+}
---
+++ src/gitrepo/main.java
@@ -0,0 +1,28 @@
+package gitrepo;
+
+public class main {
+
+	public static void main(String[] args) {
+		 // TODO Auto-generated method stub
+
+		
+		
+		
+		SalesData data = new SalesData();
+		
+		
+		displayGreeting();
+		data.display();
+	}
+	
+	private static void displayGreeting()
+	{
+		System.out.println("Hello Happy Sales People!");
+		System.out.println("This APP SHOWS SALES DATA");
+		
+		System.out.println("new stuff APP SHOWS SALES DATA");
+	}
+
+
+
+}
